6c645f003e599e811715d69ef3fbeefff0043a51
lclagorga <lclagorga@gmail.com>
2025-12-04 17:03:53 -0500

    auto update

--- daily_checks/render_daily_check.R
+++ daily_checks/render_daily_check.R
@@ -1,6 +1,6 @@
 ##this script knits the rmd
 # Set Pandoc path
 Sys.setenv(RSTUDIO_PANDOC = "C:/Program Files (x86)/pandoc-3.7.0.1")
-setwd("C:/Users/LaGorgaL.S/Documents/Chapada_Stem/daily_checks")
+setwd("C:/Users/LaGorgaL.S/Documents/Chapada_Stem/daily_checks/")
 library(rmarkdown)
 render("C:/Users/LaGorgaL.S/Documents/Chapada_Stem/daily_checks/chapada_checks.Rmd")
